6676f3433583ef85ae635bd6b064a741648343cd
AronAnand <acanand@pricesenz.com>
2025-07-26 22:30:47 +0530

    changed model

--- backend/utilities/vidyautils.py
+++ backend/utilities/vidyautils.py
@@ -10,7 +10,7 @@ client = genai.Client(api_key=GEMINI_API_KEY)
 async def create_teaching_plan(prompt_by_teacher: str):
     prompt = f"{prompt_by_teacher}. The term 1 and term 2 should be planned in detail with weekly topics, learning outcomes, and assessments. The plan should follow the CBSE syllabus and include practical and theory hours as specified in the course information. Time duration for each terms is 18 months."
     resp = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-1.5-flash",
         contents=prompt,
         config={
             "response_mime_type": "application/json",
